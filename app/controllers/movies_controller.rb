e64e0e3b707ee7766d7329d1cf004e871d6db66a
vtyagiaggies <vivektyagi.nith@gmail.com>
2015-09-23 22:28:25 -0500

    hw 2 part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,44 +15,44 @@ class MoviesController < ApplicationController
     @movies = Movie.all
     @sort_by = params[:sort_by]
     if(@sort_by != nil)
-	session[:sort_by] = @sort_by
+	    session[:sort_by] = @sort_by
     end
 
     if(params[:ratings] == nil && params[:sort_by] == nil && session[:saved_params] == nil )
-	@movies = Movie.all
-	@selected_ratings = @all_ratings
+	    @movies = Movie.all
+	    @selected_ratings = @all_ratings
     end
     if(params[:ratings] != nil || params[:sort_by] != nil)  #If parameter exist assign them to session
-	session[:saved_params] = session[:saved_params].present? ? session[:saved_params] : {}
-	if (params[:sort_by] != nil && params[:sort_by].length != 0)
-		session[:saved_params][:sort_by] = params[:sort_by]
-	elsif (session[:saved_params] != nil && session[:saved_params].length != 0)
-		params[:sort_by]  = session[:saved_params][:sort_by]
-	end
-	if (params[:ratings] != nil)	
-		session[:saved_params][:ratings] = params[:ratings]
-	end
-    elsif (session[:saved_params] != nil && session[:saved_params].length != 0) #if parameter doesn't exist but session exist redirect using session values
-	flash.keep
-	redirect_to movies_path + '?' + session[:saved_params].to_query
+	    session[:saved_params] = session[:saved_params].present? ? session[:saved_params] : {}
+	    if (params[:sort_by] != nil && params[:sort_by].length != 0)
+		    session[:saved_params][:sort_by] = params[:sort_by]
+	    elsif (session[:saved_params] != nil && session[:saved_params].length != 0)
+		    params[:sort_by]  = session[:saved_params][:sort_by]
+	    end
+	    if (params[:ratings] != nil)
+		    session[:saved_params][:ratings] = params[:ratings]
+	    end
+        elsif (session[:saved_params] != nil && session[:saved_params].length != 0) #if parameter doesn't exist but session exist redirect using session values
+	    flash.keep
+	    redirect_to movies_path + '?' + session[:saved_params].to_query
     end
 
     
     if(session[:saved_params])
     	@selected_ratings = (session[:saved_params][:ratings].present? ? session[:saved_params][:ratings].keys : @all_ratings)
-	if(@sort_by == nil)
-		@sort_by = session[:saved_params][:sort_by] ? session[:saved_params][:sort_by] : params[:sort_by]
-	end
+	    if(@sort_by == nil)
+		    @sort_by = session[:saved_params][:sort_by] ? session[:saved_params][:sort_by] : params[:sort_by]
+	    end
     else
-	@selected_ratings = @all_ratings
+	    @selected_ratings = @all_ratings
         @sort_by = params[:sort_by]
     end
 
     @sort_by = session[:sort_by]
     if(@sort_by != nil)
-	@movies = Movie.where(:rating =>@selected_ratings).order(@sort_by)
+	    @movies = Movie.where(:rating =>@selected_ratings).order(@sort_by)
     else
-	@movies = Movie.where(:rating =>@selected_ratings)
+	    @movies = Movie.where(:rating =>@selected_ratings)
     end
   end
 
